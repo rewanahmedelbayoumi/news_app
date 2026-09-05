bdda42570fb081e9a61d1e088b8f175de11df5b7
rewanbayoumi <202402652@pua.edu.eg>
2026-09-06 00:38:53 +0300

    category chip

---
+++ lib/widgets/category_chip.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class CategoryChip extends StatelessWidget {
+  final String title;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  const CategoryChip({
+    super.key,
+    required this.title,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.only(right: 10),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 12,
+        ),
+        decoration: BoxDecoration(
+          color: isSelected
+              ? Colors.black
+              : Colors.grey.shade100,
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Text(
+          title,
+          style: TextStyle(
+            color: isSelected
+                ? Colors.white
+                : Colors.black,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
